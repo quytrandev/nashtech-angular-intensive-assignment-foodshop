6255dca04cb65abf5ae90285feed54df5bc5a280
Quy Tran <quytrandev@gmail.com>
2024-08-29 12:43:57 +0700

    Add basic layout for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from "./body/body.component";
+import { HeaderComponent } from "./layout/header/header.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BodyComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class BodyComponent {
+
+}
