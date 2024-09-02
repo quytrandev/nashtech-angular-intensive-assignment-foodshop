4e2886daf2b1512a4a81d4f65e0b2df2723930e7
Quy Tran <quytrandev@gmail.com>
2024-09-02 13:26:45 +0700

    Add shop component to home/dashboard

--- src/app/body/home/home.component.ts
+++ src/app/body/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ShopComponent } from '../shop/shop.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ShopComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
