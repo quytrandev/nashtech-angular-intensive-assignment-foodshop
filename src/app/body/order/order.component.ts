258ed3269163a286c1362b81beddad2eea6cb7c8
Quy Tran <quytrandev@gmail.com>
2024-09-06 15:22:47 +0700

    Add order detail component to show order summary

--- src/app/body/order/order.component.ts
+++ src/app/body/order/order.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  orderNumber!: number;
+  submittedDate!: number;
+  orderDetails: any;
+  constructor(private cartService: CartService)
+  {
+
+  }
+  ngOnInit() {
+    this.orderNumber = this.randomIntFromInterval(1000000, 9000000);
+    this.submittedDate = Date.now();
+    this.getOrderDetails();
+  }
+
+  getOrderDetails()
+  {
+    this.cartService.getOrderDetails().pipe(first()).subscribe(details => this.orderDetails = details);
+  }
+
+  randomIntFromInterval(min: number, max: number) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
 }
